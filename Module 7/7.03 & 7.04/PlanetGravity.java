333c7e89f8d791b9afa54b0781964af5b497384e
Tile124 <tile124@icloud.com>
2018-10-09 23:19:03 -0400

    Almost Finsihed
    Almost done with the program.
    Added code to the print function and write file function

--- Module 7/7.03 & 7.04/PlanetGravity.java
+++ Module 7/7.03 & 7.04/PlanetGravity.java
@@ -1,4 +1,5 @@
-
+import java.io.IOException;
+import java.io.PrintWriter;
 /**
  * Determines the surface gravity on each planet in our solar system
  *
@@ -12,10 +13,8 @@ public class PlanetGravity
     double[] diameters = new double[]{4880, 12103.6, 12756.3, 6794, 142984, 120536, 51118, 49532};
     double[] mass = new double[]{3.30e23, 4.869e24, 5.972e24, 6.4219e23, 1.900e27, 5.68e26, 8.683e25, 1.0247e26};
     double[] convertedDiameters = convertToDiametersM(diameters);
-    for (int t = 0; t < convertedDiameters.length; t++) {
-        
-    System.out.println(convertedDiameters[t]);
-}
+    double[] surfaceGravity = calculateSurfaceGravity(mass, mass, convertedDiameters);
+    printResults();
     
 }
     
@@ -32,19 +31,36 @@ public class PlanetGravity
     }
     
     
-    public static double calculateSurfaceGravity() {
-    
-        return 0.0;
+    public static double[] calculateSurfaceGravity(double[] inputArray, double[] mass, double[] convertedDiameters) {
+        double[] surfaceGravity = new double[inputArray.length];
+        for (int c = 0; c < inputArray.length; c++) {
+            surfaceGravity[c] = ( ( (6.67 * Math.pow(10, -11)) * (mass[c]) ) /( Math.pow((convertedDiameters[c] / 2), 2 ) ) ) ;
+        }
+        
+        return surfaceGravity;
     }
-    public static void writeResults() {
-    
+    public static void writeResults(double[] surfaceGravity) throws IOException {
+        PrintWriter writeFile = new PrintWriter("PlanetResults.txt");
+        for (int a = 0; a < surfaceGravity.length; a++){
+            writeFile.println(surfaceGravity[a]);
+    }
+    writeFile.close();
     }
     
+    /*
     public static void readResults() {
     
     }
-    
+    */
+   
     public static void printResults() {
+        System.out.printf("%-23s","\n");
+        System.out.printf("%-16S", "Planetary Data");
+        System.out.printf("%-16s", "\nPlanet");
+        System.out.printf("%-16s", "Diameter(KM)");
+        System.out.printf("%-16s", "Mass(KG)");
+        System.out.printf("%-16s", "g (m/s^2)");
+        System.out.println("\n***************************************************************");
         
     }
 }
