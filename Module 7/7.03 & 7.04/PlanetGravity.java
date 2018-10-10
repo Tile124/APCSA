1142b899a113296ae18a39dbb0a409c688d1edc2
Tile124 <tile124@icloud.com>
2018-10-09 22:48:11 -0400

    PlanetGravity Update
    Coded one method

--- Module 7/7.03 & 7.04/PlanetGravity.java
+++ Module 7/7.03 & 7.04/PlanetGravity.java
@@ -11,14 +11,24 @@ public class PlanetGravity
     String[] planetNames = new String[]{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", };
     double[] diameters = new double[]{4880, 12103.6, 12756.3, 6794, 142984, 120536, 51118, 49532};
     double[] mass = new double[]{3.30e23, 4.869e24, 5.972e24, 6.4219e23, 1.900e27, 5.68e26, 8.683e25, 1.0247e26};
-    convertToDiametersM(diameters);
+    double[] convertedDiameters = convertToDiametersM(diameters);
+    for (int t = 0; t < convertedDiameters.length; t++) {
+        
+    System.out.println(convertedDiameters[t]);
+}
     
-    }
+}
     
     public static double[] convertToDiametersM(double[] arrayInput) {
+        int startLength = arrayInput.length;
+        double[] returnValue = new double[startLength];
         
-        System.out.print(arrayInput.length);
-        return arrayInput;
+        for (int i = 0; i < arrayInput.length; i++) {
+           
+            returnValue[i] = (arrayInput[i]*1000);
+            
+        }
+        return returnValue;
     }
     
     
