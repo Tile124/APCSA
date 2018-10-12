adff14d4de7078566293949b18775b358ad5e183
Ryan Rodriguez <tile124@icloud.com>
2018-10-12 11:44:59 -0400

    Program Finished

--- Module 7/7.05 Challenge Program/EstimatePi.java
+++ Module 7/7.05 Challenge Program/EstimatePi.java
@@ -11,20 +11,28 @@ public class EstimatePi
     public static void main(String[] args) {
     int dartThrows = 0;
     int trials = 0;
-    
     Scanner in = new Scanner(System.in);
     System.out.println("How many times should the Dart be thrown?");
     dartThrows = in.nextInt();
     System.out.println("How many times should the trial be run?");
     trials = in.nextInt();
-    System.out.println(trialRun(dartThrows, trials));
-        
+    double piValues[] = Pi(trialRun(dartThrows, trials),trials, dartThrows);
+    output(piValues);
     }
     
-    public static double[] trialRun(int dartThrows, int trialRuns) {
+    // Prints the pi values
+    public static void output(double[] piValues) {
+    for (int i = 0; i < piValues.length; i++) {
+    System.out.println("Trial [ " + (i+1) + " ]: pi = "+ piValues[i]);
+    
+    }
+    }
+    
+    //Simulates the trial runs
+    public static int[] trialRun(int dartThrows, int trialRuns) {
     double xValue = 0;
     double yValue = 0;
-    int numberOfHits[] = new Int[trialRuns];
+    int numberOfHits[] = new int[trialRuns];
     for (int a = 0; a < trialRuns; a++) {
      
     for (int i = 0; i < dartThrows; i++) {
@@ -39,12 +47,17 @@ public class EstimatePi
     return numberOfHits;
     }
     
-    public static double Pi(int numberOfHits, int trialRuns, int dartThrows) {
-    
-        double returnValue = (numberOfHits / (trialRuns * dartThrows))
-        return 0.0;
+    // Calculates Pi
+    public static double[] Pi(int[] numberOfHits, int trialRuns, int dartThrows) {
+    double returnValues[] = new double[numberOfHits.length];
+    for (int i = 0; i < numberOfHits.length; i++) {
+        returnValues[i] = (numberOfHits[i] / (double)dartThrows) *4;
+        
+    }
+    return returnValues;
     }
     
+    // Not needed
     /*
     public static int[] userInput() {
         Scanner in = new Scanner(System.in);
