99d057323a8abe9641ec8510c8b5a4a12c77ec39
Ryan Rodriguez <tile124@icloud.com>
2019-03-05 09:24:55 -0500

    Update

---
+++ Module 14/14.10/IntArrayWorker.java
@@ -0,0 +1,33 @@
+import java.*; 
+/**
+ * The picture explorer class does many things for this awesome lab assignment.
+ *
+ * @author Ryan A. Rodriguez
+ * @version 1.0.0, Feburary Twenty-Seventh,Twenty-nineteen
+ */
+public class IntArrayWorker
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class IntArrayWorker
+     */
+    public IntArrayWorker()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code herede923ie923ie29ie29e903e90i23ei9019i09012e9i29i19i2e9i2e129i029i129i2902199i2ejd
+        return x + y;
+    }
+}
---
+++ Module 14/14.10/IntArrayWorkerTester.java
@@ -0,0 +1,38 @@
+
+
+
+
+import java.util.*;
+import java.*; 
+/**
+ * The picture explorer class does many things for this awesome lab assignment.
+ *
+ * @author Ryan A. Rodriguez
+ * @version 1.0.0, Feburary Twenty-Seventh,Twenty-nineteen
+ */
+public class IntArrayWorkerTester
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class IntArrayWorkerTester
+     */
+    public IntArrayWorkerTester()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code herekkkkkkkdoksokdokdoi34r9i4r8920u90ud9fuds0fud092r0ue0fu83u8ruw0eru8ru03ur80rurw8ur8u384ru843ru0ru348ru348ru8430ru4830r48r043u80r3u84ru8403ru43r043r08u408ru4380ru4ru838ru480ru3r8u4308ur3408ur0834ru834ru834r83084ru03480ru834r0u834u0ru0
+        return x + y;
+    }
+}
